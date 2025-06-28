ab744dffc08008c6a019977dc5b2523116b58bec
V3R0N1C4 <120654473+V3R0N1C4@users.noreply.github.com>
2025-06-28 13:57:43 +0200

    Add project configuration files and initial .gitignore

--- MailServer/src/main/java/com/server/ServerApplication.java
+++ MailServer/src/main/java/com/server/ServerApplication.java
@@ -7,12 +7,26 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import server.controller.ServerController;
 
+import java.io.FileNotFoundException;
+import java.net.URL;
+
 public class ServerApplication extends Application {
     private ServerController controller;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        // Usa il percorso assoluto con slash iniziale
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/server/view/ServerView.fxml"));
+
+        // Codice diagnostico
+        URL fxmlUrl = getClass().getResource("/server/view/ServerView.fxml");
+        if(fxmlUrl == null) {
+            System.err.println("ERRORE: File non trovato! Cercato in: ");
+            System.err.println("Classpath: " + System.getProperty("java.class.path"));
+            throw new FileNotFoundException("ServerView.fxml non trovato");
+        }
+        System.out.println("Trovato FXML: " + fxmlUrl);
+
         Scene scene = new Scene(loader.load(), 600, 400);
 
         controller = loader.getController();
